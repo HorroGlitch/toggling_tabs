4e6881b1678095aae4bc0fb8af51d7a9abdf6f08
HorroGlitch <lverot@edenschool.fr>
2023-10-23 15:22:06 +0200

    changing color button

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-choosePrice = document.getElementById("byPrice");
-chooseName = document.getElementById("byName");
-chooseRelevance = document.getElementById("byRelevance");
+let choosePrice = document.getElementById("byPrice");
+let chooseName = document.getElementById("byName");
+let chooseRelevance = document.getElementById("byRelevance");
 
-priceButton = document.getElementById("buttonPrice");
-nameButton = document.getElementById("buttonName");
-relevanceButton = document.getElementById("buttonRelevance");
+let priceButton = document.getElementById("buttonPrice");
+let nameButton = document.getElementById("buttonName");
+let relevanceButton = document.getElementById("buttonRelevance");
+
+
+
+priceButton.addEventListener("click", ()=>{
+  priceButton.style.backgroundColor="deepskyblue";
+  nameButton.style.backgroundColor="white";
+  relevanceButton.style.backgroundColor="white";
+  choosePrice = true;
+});
+
+relevanceButton.addEventListener("click", ()=>{
+  relevanceButton.style.backgroundColor="deepskyblue";
+  nameButton.style.backgroundColor="white";
+  priceButton.style.backgroundColor="white";
+});
+
+nameButton.addEventListener("click", ()=>{
+  nameButton.style.backgroundColor="deepskyblue";
+  priceButton.style.backgroundColor="white";
+  relevanceButton.style.backgroundColor="white";
+});
+
 
