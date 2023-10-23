fc4c02dfb274e05524a08b76fb42b0a5fb9f29c1
HorroGlitch <lverot@edenschool.fr>
2023-10-23 14:44:25 +0200

    creation du squelette

---
+++ main.js
@@ -0,0 +1,8 @@
+choosePrice = document.getElementById("byPrice");
+chooseName = document.getElementById("byName");
+chooseRelevance = document.getElementById("byRelevance");
+
+priceButton = document.getElementById("buttonPrice");
+nameButton = document.getElementById("buttonName");
+relevanceButton = document.getElementById("buttonRelevance");
+
